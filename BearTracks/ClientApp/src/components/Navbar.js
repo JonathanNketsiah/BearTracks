844da76141dc5dcaf69ecfec8acb03f5dcdff661
jona nket <jnketsiah@ggc.edu>
2023-10-30 14:04:50 -0400

    added icons to navbar

--- BearTracks/ClientApp/src/components/Navbar.js
+++ BearTracks/ClientApp/src/components/Navbar.js
@@ -2,6 +2,11 @@ import './NavBarStyle.css'
 import { NavLink } from "react-router-dom"
 import { useNavigate } from 'react-router-dom';
 import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import Diversity1Icon from '@mui/icons-material/Diversity1';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import InfoIcon from '@mui/icons-material/Info';
+
 
 
 
@@ -15,13 +20,13 @@ function Navbar() {
     return (
         <>
             <nav>
-                <NavLink to="/Home" className="title" > BearTracks</NavLink>
+                <NavLink to="/Home" className="title" >  <HomeIcon fontSize="large" /> BearTracks</NavLink>
                
                 <div>
                     <ul id="navbar">                           
-                        <li><NavLink to="/Social" className="link" >Social</NavLink></li>
-                        <li><NavLink to="/Profile" className="link" >Account</NavLink></li>
-                        <li><NavLink to="/About" className="link" >About us</NavLink></li>
+                        <li><NavLink to="/Social" className="link" > <Diversity1Icon fontSize="large" /> Social</NavLink></li>
+                        <li><NavLink to="/Profile" className="link" > <AccountCircleIcon fontSize="large"/>  Account</NavLink></li>
+                        <li><NavLink to="/About" className="link" ><InfoIcon fontSize="large"/> About us</NavLink></li>
                         <button className ="logOutbutton"onClick={LogOut}>LogOut </button>
                         
                        
