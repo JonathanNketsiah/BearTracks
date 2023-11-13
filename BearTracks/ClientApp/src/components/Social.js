eb608317f1d0b3d4c2d26987df3eb827b0281856
JonathanNketsiah <jnketsiah@ggc.edu>
2023-11-12 19:05:49 -0500

    Revert "populated social page with hardcoded data and modified navbar"
    This reverts commit 0551366e24cbb2184175746c316039074efda3f5.

--- BearTracks/ClientApp/src/components/Social.js
+++ BearTracks/ClientApp/src/components/Social.js
@@ -1,99 +1,15 @@
 import React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Bear from './Assets/bearprofile.jpg'
-import Jane from './Assets/Jane.jpg'
-import John from './Assets/John.jpg'
-import Mary from './Assets/Mary.jpg'
-import Travis from './Assets/Travis.jpg'
-//import defaultAvatar from './Assets/defaultAvatar.png'
-import './LoginFront.css'
+import { useNavigate } from 'react-router-dom';
 
 
 const Social = () => {
-   
 
 
     return (
-        <>  
-            
-            <div className="socialContainer">
-                <h2>Friend's List</h2>
-                <div className="socialCards">
-                <div className="pborder">
-                        <Avatar className="imgDp" sx={{ width: 100, height: 100 }} src={Jane} />
-                    </div>
-                    <div className="contents">
-                    <div className="textDoc">
-                    <h5>Jane lane</h5>
-                        <label>Interest: Soccer,Bowling , Fahsion</label>
-                            <label> Recent visit: SCAD Musuem</label>
-                        </div>
-                    </div>
+        <>
+            <h1>Social</h1>
 
-                </div>
-
-                <div className="socialCards">
-                    <div className="pborder">
-                        <Avatar className="imgDp" sx={{ width: 100, height: 100 }} src={John} />
-                    </div>
-                    <div className="contents">
-                        <div className="textDoc">
-                            <h5>John Henry</h5>
-                            <label>Interest: Dancing,concerts,hockey</label>
-                            <label> Recent visit: Astroworld concert</label>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="socialCards">
-                    <div className="pborder">
-                        <Avatar className="imgDp" sx={{ width: 100, height: 100 }} src={Travis} />
-                    </div>
-                    <div className="contents">
-                        <div className="textDoc">
-                            <h5>Travis Gardner</h5>
-                            <label>Interest: Basketball,Art gallery,Movies</label>
-                            <label> Recent visit:AMC colonial</label>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="socialCards">
-                    <div className="pborder">
-                        <Avatar className="imgDp" sx={{ width: 100, height: 100 }} src={Mary} />
-                    </div>
-                    <div className="contents">
-                        <div className="textDoc">
-                            <h5>Mary Hange</h5>
-                            <label>Interest: Theatre,Fun fair, Malls</label>
-                            <label> Recent visit: Lenox Square </label>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="socialCards">
-                    <div className="pborder">
-                        <Avatar className="imgDp" sx={{ width: 100, height: 100 }} src={Bear} />
-                    </div>
-                    <div className="contents">
-                        <div className="textDoc">
-                            <h5>Sarah Rain</h5>
-                            <label>Interest: Concerts,Marathons,Arcade</label>
-                            <label> Recent visit: Renaissance World Tour Concert</label>
-                        </div>
-                    </div>
-
-                </div>
-
-                
-
-            </div>
         </>
-
-
     )
 
 
