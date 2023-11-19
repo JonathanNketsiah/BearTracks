a691e5d2c14557015526f81696d9afb2bfcb139b
ChrisT <c-titus@hotmail.com>
2023-11-19 17:15:20 -0500

    time set to 3sec

--- BearTracks/ClientApp/src/App.js
+++ BearTracks/ClientApp/src/App.js
@@ -8,7 +8,11 @@ import Navbar from './components/Navbar'
 import Profile from './components/Profile'
 import Social from './components/Social'
 import About from './components/About'
+<<<<<<< HEAD
 import React, { useEffect, useState, CSSProperties } from 'react'
+=======
+import React, { useEffect, useState } from 'react'
+>>>>>>> 7087262010b003c1a17d32a627e50c7dff6384df
 import NavbarStatus from './components/NavbarStatus'
 import ClipLoader from "react-spinners/ClipLoader"
 
@@ -20,9 +24,14 @@ const override: CSSProperties = {
 function App() {
    
     const [loading, setLoading] = useState(false)
+<<<<<<< HEAD
     useEffect(() => {
             // change to on execute for buttons
         setLoading(true)
+=======
+        useEffect(() => {
+            setLoading(true)
+>>>>>>> 7087262010b003c1a17d32a627e50c7dff6384df
             setTimeout(() => { setLoading(false) }, 3000)
          }, [])
     return (
@@ -30,6 +39,7 @@ function App() {
             <NavbarStatus>
                 <Navbar />
             </NavbarStatus>
+<<<<<<< HEAD
             {
                 loading ? <ClipLoader color={'#D0021B'} loading={loading} cssOverride={override} size={200} /> :
                     <Routes>
@@ -45,6 +55,23 @@ function App() {
                     </Routes>
 
             }
+=======
+
+            loading? <ClipLoader color={'#D0021B'} loading={loading} size={100} /> :
+            <Routes>
+                <Route path='/' element={<Login />} />
+                <Route path='/signUp' element={<SignUp />} />
+                <Route path='/Home' element={<MapPage />} />
+                <Route path='/Profile' element={<Profile />} />
+                <Route path='/Social' element={<Social />} />
+                <Route path='/About' element={<About />} />
+                <Route path='/LandingPage' element={<LandingPage/>} />
+                <Route element={<ProtectedRoutes/> } >
+                </Route>
+            </Routes>
+         
+           
+>>>>>>> 7087262010b003c1a17d32a627e50c7dff6384df
         </>
     )
 }
